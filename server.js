c25c5d65033812b303aafb487d80b087b9fb5a3b
manish kumar <manishgga09@gmail.com>
2024-02-26 02:27:30 +0530

    full stack setup

---
+++ server.js
@@ -0,0 +1,63 @@
+const express= require("express");
+const app=express();
+const port=3000;
+const {MongoClient} = require("mongodb")
+
+
+
+
+//initialize middleware 
+//we use to have to install body parser but now it is a built in middleware
+//
+app.use(express.json({extended:false}))
+
+const withDB = async (operations, res) => {
+    try {
+      const client = await MongoClient.connect("mongodb://localhost:27017");
+      const db = client.db("mernblog");
+      await operations(db);
+      client.close();
+    } catch (error) {
+      res.status(500).json({ message: "Error connecting to database", error });
+    }
+  };
+
+  app.get("/api/articles/:name", async (req, res) => {
+    withDB(async (db) => {
+      const articleName = req.params.name;
+      const articleInfo = await db
+        .collection("articles")
+        .findOne({ name: articleName });
+      res.status(200).json(articleInfo);
+    }, res);
+  });
+  
+  app.post("/api/articles/:name/add-comments", (req, res) => {
+    const { username, text } = req.body;
+    const articleName = req.params.name;
+  
+    withDB(async (db) => {
+      const articleInfo = await db
+        .collection("articles")
+        .findOne({ name: articleName });
+      await db.collection("articles").updateOne(
+        { name: articleName },
+        {
+          $set: {
+            comments: articleInfo.comments.concat({ username, text }),
+          },
+        }
+      );
+      const updateAricleInfo = await db
+        .collection("articles")
+        .findOne({ name: articleName });
+      res.status(200).json(updateAricleInfo);
+    }, res);
+  });
+  
+app.listen(port,(err)=>{
+    if(!err){
+        console.log("running at "+port);
+    }
+})
+ 
